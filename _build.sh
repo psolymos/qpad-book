32952cd844306a51843249d8075be328cf62e9a5
Peter Solymos <psolymos@gmail.com>
2019-06-06 21:54:40 -0600

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -2,7 +2,6 @@
 
 set -ev
 
-Rscript -e "bookdown::clean_book(TRUE)"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
